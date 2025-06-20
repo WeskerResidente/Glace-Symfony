d2bda2f135c04c2f38ec04e7cce5a8ca5d7b903d
Norman BELAID <Normanbelaid@gmail.com>
2025-06-20 16:47:54 +0200

    add problems

---
+++ src/Controller/Admin/GlaceAdminCrudController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Glace;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+
+class GlaceAdminCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Glace::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    
+}
