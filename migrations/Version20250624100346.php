528765e0ffe729c77e9015e7961567930c849fb0
Norman BELAID <normanbelaid@gmail.com>
2025-06-24 11:14:52 +0100

    problem img

--- migrations/Version20250624100346.php
+++ migrations/Version20250624100346.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250620074138 extends AbstractMigration
+final class Version20250624100346 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,7 +21,7 @@ final class Version20250620074138 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            ALTER TABLE glace CHANGE ingredient_special ingredient_special VARCHAR(255) DEFAULT NULL
+            ALTER TABLE glace DROP image
         SQL);
     }
 
@@ -29,7 +29,7 @@ final class Version20250620074138 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            ALTER TABLE glace CHANGE ingredient_special ingredient_special VARCHAR(255) NOT NULL
+            ALTER TABLE glace ADD image VARCHAR(255) NOT NULL
         SQL);
     }
 }
--- src/Entity/Glace.php
+++ src/Entity/Glace.php
@@ -2,15 +2,30 @@
 
 namespace App\Entity;
 
-use App\Repository\GlaceRepository;
+use DateTimeImmutable;
+use Vich\UploadableField;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\GlaceRepository;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
+#[Vich\Uploadable]
 #[ORM\Entity(repositoryClass: GlaceRepository::class)]
 class Glace
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Vich\UploadableField(mapping: 'images', fileNameProperty: 'imageName')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $imageName = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?DateTimeImmutable $updatedAt = null;
+
+    #[ORM\Column(type:"integer")]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
@@ -62,4 +77,31 @@ class Glace
 
         return $this;
     }
+
+        public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if ($imageFile) {
+            // Si un fichier est chargé, met à jour la date
+            $this->updatedAt = new DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+
 }
